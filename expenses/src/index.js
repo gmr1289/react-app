d3c5d4588ccebef183003c1cd59faf284e88ad27
gmr1289 <gmrgamer1289@gmail.com>
2022-06-10 19:40:57 +0530

    del

--- expenses/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/cjs/react-dom.production.min";
-import App from "./App";
-import "./index.css";
-const root = createRoot(document.getElementById("root"))
-root.render(<App />);
